83e772e53547a248e81b2194b2cb4617cfa676cd
otegaa <ekiugbotega@gmail.com>
2023-06-15 13:50:35 +0200

    Fix linters errors

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -3,51 +3,51 @@ def decode_char(morse_char)
 end
 
 def decode_word(morse_word)
-  word = morse_word.split(" ").map { |char| MORSE_CODE[char] }.join
+  morse_word.split.map { |char| MORSE_CODE[char] }.join
 end
 
 def decode_with_3_spaces(morse_word)
-  words = morse_word.split("   ").map { |char| decode_word(char) }
-  message = words.join(" ")
+  words = morse_word.split('   ').map { |char| decode_word(char) }
+  words.join(' ')
 end
 
 MORSE_CODE = {
-".-" => "A",
-"-..." => "B",
-"-.-." => "C",
-"-.." => "D",
-"." => "E",
-"..-." => "F",
-"--." => "G",
-"...." => "H",
-".." => "I",
-".---" => "J",
-"-.-" => "K",
-".-.." => "L",
-"--" => "M",
-"-." => "N",
-"---" => "O",
-".--." => "P",
-"--.-" => "Q",
-".-." => "R",
-"..." => "S",
-"-" => "T",
-"..-" => "U",
-"...-" => "V",
-".--" => "W",
-"-..-" => "X",
-"-.--" => "Y",
-"--.." => "Z"
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
-decoded_char = decode_char (".-")
+decoded_char = decode_char('.-')
 puts decoded_char
 
-decoded_word = decode_word("-- -.--")
+decoded_word = decode_word('-- -.--')
 puts decoded_word
 
-decoded_str = decode_with_3_spaces("-- -.--   -. .- -- .")
+decoded_str = decode_with_3_spaces('-- -.--   -. .- -- .')
 puts decoded_str
 
-decoded_str2 = decode_with_3_spaces(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+decoded_str2 = decode_with_3_spaces('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 puts decoded_str2
