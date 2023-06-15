b2a66fe764578802604cc0e991ca7d7a537dae13
otegaa <ekiugbotega@gmail.com>
2023-06-15 13:12:27 +0200

    Made a method for taking in strings with three spaces

--- decode_morse_code.rb
+++ decode_morse_code.rb
@@ -3,8 +3,12 @@ def decode_char(morse_char)
 end
 
 def decode_word(morse_word)
-word = morse_word.split(" ").map { |char| MORSE_CODE[char] }.join
+  word = morse_word.split(" ").map { |char| MORSE_CODE[char] }.join
 end
 
+def decode_with_3_spaces(morse_word)
+  words = morse_word.split(" ").map { |char| decode_word(char) }
+  message = words.join(" ")
+end
 
 
